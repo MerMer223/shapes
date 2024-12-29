53e1955638e021e7b91a8595b2f155bd8e45395c
MerMer223 <mehraabjhajj123@icloud.com>
2024-12-29 10:13:25 -0500

    Add files via upload

---
+++ competition.py
@@ -0,0 +1,15 @@
+groupname = input("What is the name of your group")
+sizeofthegroup = input("How many people are in your group")
+dateofthecompetition = input("When did the competition take place")
+venue = input("Where did the competition take place")
+typeofthemedal = input("What is the reward of the compeition")
+
+Group1 = (groupname,sizeofthegroup,dateofthecompetition,venue,typeofthemedal)
+
+
+
+print(Group1)
+
+
+
+
