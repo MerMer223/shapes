efe81c72e9cbd3156fa4d477f986d79b0abc9d00
MerMer223 <mehraabjhajj123@icloud.com>
2025-01-05 10:56:58 -0500

    Add files via upload

--- spacegame.py
+++ spacegame.py
@@ -1,5 +1,6 @@
 import pgzrun
 import random
+import time
 WIDTH = 1000
 HEIGHT = 800
 
@@ -9,16 +10,23 @@ next = 0
 
 lines = []
 satelitenum = 10
+start = 0
+total = 0
 for i in range (satelitenum):
     satelite = Actor("satelite")
     satelite.x = random.randint(100,900)
     satelite.y = random.randint(100,700)
     sats.append(satelite)
 
-
+start = time.time()
 def draw():
+    global start
+    global total
     screen.blit("space",(0,0))
     num = 1
+    if next < satelitenum:
+        total = time.time()-start
+    screen.draw.text(str(round(total,2)),(500,30))
     for i in sats:
         i.draw()
         screen.draw.text(str(num), (i.x, i.y - 25))
@@ -35,6 +43,9 @@ def on_mouse_down(pos):
                 lines.append((sats[next-1].pos,sats[next].pos))
             next = next + 1
             print(lines)
+        else:
+                lines = []
+                next = 0
 
 
 def update():
