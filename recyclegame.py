3c448d7f28f2d6bb9c5b48848c5b06bbfe1c16ad
MerMer223 <mehraabjhajj123@icloud.com>
2025-02-23 10:46:53 -0500

    Add files via upload

--- recyclegame.py
+++ recyclegame.py
@@ -13,7 +13,7 @@ Gamecomplete = False
 Currentlevel = 1
 Items = []
 Animations = []
-
+startspeed = 12
 
 
 def draw():
@@ -39,19 +39,75 @@ def MakeItems(no_of_items):
     ItemsToCreate = randomitems(no_of_items)
     NewItems = CreateItems(ItemsToCreate)
     Layout(NewItems)
-    Animate(Items)
+    Animate(NewItems)
+    return NewItems
+def randomitems(no_of_items):
+    ItemsToCreate = ["paperbag"]
+    for i in range(no_of_items):
+        name = random.choice(Nonrecycle)
+        ItemsToCreate.append(name)
+    return ItemsToCreate
+
+def CreateItems(ItemsToCreate):
+    NewItems = []
+    for items in ItemsToCreate:
+        item = Actor(items)
+        NewItems.append(item)
     return NewItems
 
+def Layout(NewItems):
+    gap = len(NewItems) + 1
+    gapsize = WIDTH/gap
+    random.shuffle(NewItems)
+    for index,item in enumerate(NewItems):
+        x_pos = (index+1)*gapsize
+        item.x=x_pos
+
+def Animate(NewItems):
+    global Animations
+    global Currentlevel
+    print(NewItems)
+    for item in NewItems:
+        item.anchor = ("center","bottom")
+        dur = startspeed - Currentlevel
+        Animation = animate(item,duration=dur,y=HEIGHT,on_finished=gameover)
+        Animations.append(Animation)
+
+def gameover():
+    global Gameover
+    Gameover = True
 
 
 def on_mouse_down(pos):
-   # if paperbag.collidepoint(pos):
-   pass
+   global Items,Currentlevel
+   for item in Items:
+        if item.collidepoint(pos):
+            if item.image == "paperbag":
+                Game_complete()
+            else:
+                gameover()
+   
+def Game_complete():
+    global Currentlevel,Items,Animations,Gamecomplete
+    for animation in Animations:
+        if animation.running:
+            animation.stop()
+    if Currentlevel == Level:
+        Gamecomplete = True
+    else:
+        Currentlevel = Currentlevel +1
+        Items = []
+        Animations = []
 
 
 
+def shuffleitems():
+    global Items
+    if Items:
+        Layout(Items)
 
 
+clock.schedule_interval(shuffleitems,2)
 
 
 
